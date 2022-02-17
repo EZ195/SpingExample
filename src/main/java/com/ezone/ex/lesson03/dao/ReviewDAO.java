3beda629a3cebe32172ca34d159c4a95d9042ea1
cheong <cheong@naver.com>
2022-02-17 18:05:41 +0900

    22-02-17 업로드

---
+++ src/main/java/com/ezone/ex/lesson03/dao/ReviewDAO.java
@@ -0,0 +1,14 @@
+package com.ezone.ex.lesson03.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ezone.ex.lesson03.model.Review;
+
+public interface ReviewDAO {
+	
+	
+	//id 값을 전달 받아서 해당하는 리뷰 값을 리턴하다.
+	public Review selectReview(@Param("id")int id);
+	
+}
