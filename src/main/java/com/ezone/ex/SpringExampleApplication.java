cc2a1f5cb00e9f94ea6ded04d3d2e91ca984984d
LAPTOP-JPK2Q002\EZi09 <95jiwon91@gmail.com>
2022-07-23 01:11:19 +0900

    update

--- src/main/java/com/ezone/ex/SpringExampleApplication.java
+++ src/main/java/com/ezone/ex/SpringExampleApplication.java
@@ -7,14 +7,9 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @SpringBootApplication
-public class SpringExampleApplication {
+public class SpringExampleApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
 	}
